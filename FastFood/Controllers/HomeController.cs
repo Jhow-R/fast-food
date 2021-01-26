435c88f4e8ec4ba020cdc04552d480d2c232315c
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-01-26 12:42:12 -0300

    View de lanches preferidos

--- FastFood/Controllers/HomeController.cs
+++ FastFood/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FastFood.Data.Repository.Interfaces;
+using FastFood.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace FastFood.Controllers
@@ -6,15 +8,22 @@ namespace FastFood.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ILancheRepository _lancheRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ILancheRepository lancheRepository)
         {
             _logger = logger;
+            _lancheRepository = lancheRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesFavoritos = _lancheRepository.LanchesFavoritos
+            };
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ FastFood/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FastFood.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesFavoritos { get; set; }
+    }
+}
