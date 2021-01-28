823c49ff19f22b7334b6ae10a58f83227cb0ed9d
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-01-28 15:24:34 -0300

    Controller de pedidos

---
+++ FastFood/Controllers/PedidosController.cs
@@ -0,0 +1,48 @@
+﻿using FastFood.Data.Repository.Interfaces;
+using FastFood.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FastFood.Controllers
+{
+    public class PedidosController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompras _carrinhoCompras;
+
+        public PedidosController(IPedidoRepository pedidoRepository, CarrinhoCompras carrinhoCompras)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompras = carrinhoCompras;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            var items = _carrinhoCompras.GetCarrinhoCompraItens();
+            _carrinhoCompras.CarrinhoCompraItens = items;
+
+            if (_carrinhoCompras.CarrinhoCompraItens.Count == 0)
+                ModelState.AddModelError(String.Empty, "Seu carrinho está vazio");
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+                _carrinhoCompras.LimparCarrinho();
+                return RedirectToAction(nameof(CheckoutCompleto));
+            }
+
+            return View(pedido);
+        }
+
+        public IActionResult CheckoutCompleto()
+        {
+            return View();
+        }
+    }
+}
