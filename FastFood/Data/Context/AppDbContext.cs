2742964d44ddf042e902c68947132cebc32ae874
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-01-28 11:23:53 -0300

    Migration de Pedidos

--- FastFood/Data/Context/AppDbContext.cs
+++ FastFood/Data/Context/AppDbContext.cs
@@ -10,5 +10,7 @@ namespace FastFood.Data.Context
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
