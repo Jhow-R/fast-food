1b7f32874f811eab6de23fd9a1558a04a4afd00a
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-03-30 15:30:34 -0300

    Implementação do Login

---
+++ FastFood/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using FastFood.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace FastFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid is false)
+                return View(viewModel);
+
+            var user = await _userManager.FindByNameAsync(viewModel.Username);
+
+            if (user is not null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    user, viewModel.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    if (String.IsNullOrEmpty(viewModel.ReturnUrl))
+                        return RedirectToAction(nameof(Index), nameof(HomeController));
+
+                    return RedirectToAction(viewModel.ReturnUrl);
+                }
+            }
+
+            ModelState.AddModelError(String.Empty, "Credencias inválidas ou não localizadas");
+            return View(viewModel);
+        }
+    }
+}
---
+++ FastFood/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário")]
+        public String Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        public String ReturnUrl { get; set; }
+    }
+}
