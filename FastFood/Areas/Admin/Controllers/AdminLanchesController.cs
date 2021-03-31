3da7e87d7a017648168ee0c2d09626c166d3349f
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-03-31 13:01:36 -0300

    Controllers admin de lanches, pedidos e categorias

---
+++ FastFood/Areas/Admin/Controllers/AdminLanchesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FastFood.Data.Context;
+using FastFood.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FastFood.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class AdminLanchesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminLanchesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Lanches
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Lanches.Include(l => l.Categoria);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Lanches/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lanche = await _context.Lanches
+                .Include(l => l.Categoria)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lanche == null)
+            {
+                return NotFound();
+            }
+
+            return View(lanche);
+        }
+
+        // GET: Admin/Lanches/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Lanches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,DescricaoCurta,DescricaoDetalhada,Preco,ImagemUrl,ImagemThumbnailUrl,IsLancheFavorito,EmEstoque,CategoriaId")] Lanche lanche)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lanche);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Id", lanche.CategoriaId);
+            return View(lanche);
+        }
+
+        // GET: Admin/Lanches/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lanche = await _context.Lanches.FindAsync(id);
+            if (lanche == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Id", lanche.CategoriaId);
+            return View(lanche);
+        }
+
+        // POST: Admin/Lanches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DescricaoCurta,DescricaoDetalhada,Preco,ImagemUrl,ImagemThumbnailUrl,IsLancheFavorito,EmEstoque,CategoriaId")] Lanche lanche)
+        {
+            if (id != lanche.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lanche);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LancheExists(lanche.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Id", lanche.CategoriaId);
+            return View(lanche);
+        }
+
+        // GET: Admin/Lanches/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lanche = await _context.Lanches
+                .Include(l => l.Categoria)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lanche == null)
+            {
+                return NotFound();
+            }
+
+            return View(lanche);
+        }
+
+        // POST: Admin/Lanches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var lanche = await _context.Lanches.FindAsync(id);
+            _context.Lanches.Remove(lanche);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LancheExists(int id)
+        {
+            return _context.Lanches.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FastFood/Areas/Admin/Controllers/AdminPedidosController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FastFood.Data.Context;
+using FastFood.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FastFood.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class AdminPedidosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminPedidosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Pedidoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pedidos.ToListAsync());
+        }
+
+        // GET: Admin/Pedidoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // GET: Admin/Pedidoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Pedidoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Sobrenome,Endereco1,Endereco2,Cep,Estado,Cidade,Telefone,Email,PedidoEnviado,PedidoEntregueEm")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pedido);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pedido);
+        }
+
+        // GET: Admin/Pedidoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            return View(pedido);
+        }
+
+        // POST: Admin/Pedidoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Sobrenome,Endereco1,Endereco2,Cep,Estado,Cidade,Telefone,Email,PedidoEnviado,PedidoEntregueEm")] Pedido pedido)
+        {
+            if (id != pedido.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pedido);
+        }
+
+        // GET: Admin/Pedidoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // POST: Admin/Pedidoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedido = await _context.Pedidos.FindAsync(id);
+            _context.Pedidos.Remove(pedido);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedidos.Any(e => e.Id == id);
+        }
+    }
+}
